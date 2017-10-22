fda47fce788be630bcd0d515a76e3aada7d3f4b6
yangdanny97 <dzy4@cornell.edu>
2017-10-22 17:06:44 -0400

    added run and some more fields

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import praw
+import tldextract
 from newspaper import Article
 from summary import Summarizer
 
@@ -10,38 +11,49 @@ class Bot:
         def __init__(self):
             self.reddit = praw.Reddit(client_id=os.environ.get('CLIENT_ID'),
                          client_secret=os.environ.get('CLIENT_SECRET'),
-                         user_agent="user_agent"
+                         user_agent="News Article Summary Bot Test"
                          username=os.environ.get('BOT_USERNAME'),
                          password=os.environ.get('BOT_PASSWORD'))
+            self.subreddits_list = []
+            self.approved_sites = set([])
             self.subreddits = reddit.subreddit('Test')
 
+
         def run(self):
-            pass
-
-
+            for submission in subreddits.stream.submissions():
+                url_check = tldextract.extract(submission.extract).domain
+                proceed = False
+                for site in self.approved_sites:
+                    if url_check in self.approved_sites: proceed = True
+                if proceed:
+                    self.create_summary(submission.url)
 
         """
         Summarizes the article contained within the given url
-
         """
         def create_summary(self, url, shrinker=5):
-                #1. parse conents of link - don't do anything if the link is bad/the contents can't be parsed
-                #2. create instance of Summarizer
-                #3. return output of Summarizer
-                text = Bot.get_article_text(url)
-                summarizer = Summarizer(text)
-                summary = summarizer.summarize(len(summarizer.sentences)/shrinker)
-                return summary
+            #1. parse conents of link - don't do anything if the link is bad/the contents can't be parsed
+            #2. create instance of Summarizer
+            #3. return output of Summarizer, or empty string if any step fails
+            text = Bot.get_article_text(url)
+            summarizer = Summarizer(text)
+            if text=="" or len(summarizer.sentences)<16:
+                return ""
+            summary = summarizer.summarize(len(summarizer.sentences)/shrinker)
+            return summary
 
         @staticmethod
         def get_article_text(url):
             """
             Gets the article text of a given url
             """
-            article = Article(url)
-            article.download()
-            article.parse()
-            return article.text
+            try:
+                article = Article(url)
+                article.download()
+                article.parse()
+                return article.text
+            except:
+                return ""
 
         """
         Writes a reddit post with the summary
